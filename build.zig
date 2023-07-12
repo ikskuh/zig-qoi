0da395ad12ee573df6ab54d5dafa3695a1a8f589
steza73 <blink3090@gmail.com>
2023-07-12 20:17:01 +0500

    fix build.zig

--- build.zig
+++ build.zig
@@ -1,65 +1,57 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    const mode = b.standardReleaseOptions();
+    const optimization = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const converter = b.addExecutable("qoi-convert", "src/convert.zig");
-    converter.setBuildMode(mode);
-    converter.setTarget(target);
-    converter.addPackage(.{
-        .name = "args",
-        .source = .{ .path = "vendor/zig-args/args.zig" },
+    var converter = b.addExecutable(.{
+        .name = "qoi-convert",
+        .root_source_file = .{ .path = "src/convert.zig" },
+        .target = target,
+        .optimize = optimization,
     });
-    converter.addPackage(.{
-        .name = "qoi",
-        .source = .{ .path = "src/qoi.zig" },
-    });
-    converter.addPackage(.{
-        .name = "img",
-        .source = .{ .path = "vendor/zigimg/zigimg.zig" },
-    });
-    converter.install();
 
-    const benchmark = b.addExecutable("qoi-bench", "src/bench.zig");
-    benchmark.setBuildMode(mode);
-    benchmark.setTarget(target);
-    benchmark.addPackage(.{
-        .name = "args",
-        .source = .{ .path = "vendor/zig-args/args.zig" },
+    converter.addAnonymousModule("args", .{ .source_file = .{ .path = "vendor/zig-args/args.zig" } });
+    converter.addAnonymousModule("qoi", .{ .source_file = .{ .path = "src/qoi.zig" } });
+    converter.addAnonymousModule("img", .{ .source_file = .{ .path = "vendor/zigimg/zigimg.zig" } });
+    b.installArtifact(converter);
+
+    var benchmark = b.addExecutable(.{
+        .name = "qoi-bench",
+        .root_source_file = .{ .path = "src/bench.zig" },
+        .target = target,
+        .optimize = optimization,
     });
+
+    benchmark.addAnonymousModule("args", .{ .source_file = .{ .path = "vendor/zig-args/args.zig" } });
     benchmark.linkLibC();
-    benchmark.install();
+    b.installArtifact(benchmark);
 
-    const benchmark_files = b.addExecutable("qoi-bench-files", "src/bench-files.zig");
-    benchmark_files.setBuildMode(mode);
-    benchmark_files.setTarget(target);
-    benchmark_files.addPackage(.{
-        .name = "args",
-        .source = .{ .path = "vendor/zig-args/args.zig" },
+    var benchmark_files = b.addExecutable(.{
+        .name = "qoi-bench-files",
+        .root_source_file = .{ .path = "src/bench-files.zig" },
+        .target = target,
+        .optimize = optimization,
     });
-    benchmark_files.addPackage(.{
-        .name = "qoi",
-        .source = .{ .path = "src/qoi.zig" },
-    });
-    benchmark_files.addPackage(.{
-        .name = "img",
-        .source = .{ .path = "vendor/zigimg/zigimg.zig" },
-    });
+    benchmark_files.addAnonymousModule("args", .{ .source_file = .{ .path = "vendor/zig-args/args.zig" } });
+    benchmark_files.addAnonymousModule("qoi", .{ .source_file = .{ .path = "src/qoi.zig" } });
+    benchmark_files.addAnonymousModule("img", .{ .source_file = .{ .path = "vendor/zigimg/zigimg.zig" } });
     benchmark_files.linkLibC();
-    benchmark_files.install();
+    b.installArtifact(benchmark_files);
 
     const test_step = b.step("test", "Runs the test suite.");
     {
-        const test_runner = b.addTest("src/qoi.zig");
-        test_runner.setBuildMode(mode);
+        const test_runner = b.addTest(.{
+            .root_source_file = .{ .path = "src/qoi.zig" },
+            .target = target,
+            .optimize = optimization,
+        });
         test_step.dependOn(&test_runner.step);
     }
 
     const benchmark_step = b.step("benchmark", "Runs the benchmark.");
     {
-        const runner = benchmark.run();
-
+        const runner = b.addRunArtifact(benchmark);
         benchmark_step.dependOn(&runner.step);
     }
 }
